e7f8524ef45be1425e5f58160c71c28e2f7c0385
Cole Blakley <cblakley15@gmail.com>
2025-12-28 15:04:07 -0700

    Changed driver to only accept one unit (the main unit)
    Other units will be parsed/compiled on demand based on the dependency graph starting
    from the main unit. In future will need to add command-line flags to include search directories
    and mapping files between package names and filenames

--- src/main.c
+++ src/main.c
@@ -29,7 +29,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 static
 void usage(const char* exe)
 {
-    fprintf(stderr, "Usage: %s file...\n", exe);
+    fprintf(stderr, "Usage: %s main_unit\n", exe);
 }
 
 static
@@ -42,42 +42,40 @@ bool ends_with(StringView s, const char* suffix)
 
 int main(int argc, char** argv)
 {
-    if(argc < 2) {
+    if(argc != 2) {
         usage(argv[0]);
         return 1;
     }
 
     string_pool_init();
     parser_init();
-    for(int i = 1; i < argc; ++i) {
-        const char* source_file_path = argv[i];
-        StringView source_file_path_view = {.value = source_file_path, .len = strlen(source_file_path)};
+    const char* source_file_path = argv[1];
+    StringView source_file_path_view = {.value = source_file_path, .len = strlen(source_file_path)};
 
-        if(ends_with(source_file_path_view, ".ads")) {
-            fprintf(stderr, "Error: Specification files are parsed on demand as needed - no need to pass %s explicitly\n", source_file_path);
-            return 1;
-        }
-        if(!ends_with(source_file_path_view, ".adb")) {
-            fprintf(stderr, "Error: Unrecognized source file extension (must be .adb)\n");
-            return 1;
-        }
+    if(ends_with(source_file_path_view, ".ads")) {
+        fprintf(stderr, "Error: Specification files are parsed on demand as needed - no need to pass %s explicitly\n", source_file_path);
+        return 1;
+    }
+    if(!ends_with(source_file_path_view, ".adb")) {
+        fprintf(stderr, "Error: Unrecognized source file extension (must be .adb)\n");
+        return 1;
+    }
 
-        FileBuffer file_buffer;
-        if(!file_buffer_open(&file_buffer, source_file_path)) {
-            return 1;
-        }
-        error_set_source_file_path(source_file_path);
-        CompilationUnit* unit = parser_parse(file_buffer.start, file_buffer.end);
-        if(unit == NULL) {
-            fprintf(stderr, "Error: Failed to parse %s\n", source_file_path);
-            return 1;
-        }
-        print_compilation_unit(unit);
-        putchar('\n');
+    FileBuffer file_buffer;
+    if(!file_buffer_open(&file_buffer, source_file_path)) {
+        return 1;
+    }
+    error_set_source_file_path(source_file_path);
+    CompilationUnit* main_unit = parser_parse(file_buffer.start, file_buffer.end);
+    if(main_unit == NULL) {
+        fprintf(stderr, "Error: Failed to parse %s\n", source_file_path);
+        return 1;
+    }
+    print_compilation_unit(main_unit);
+    putchar('\n');
 
-        if(!file_buffer_close(&file_buffer)) {
-            return 1;
-        }
+    if(!file_buffer_close(&file_buffer)) {
+        return 1;
     }
 
     return 0;
