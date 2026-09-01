6f49c9f42a109340ef2c9dabdf83fdca44f3b74a
Cole Blakley <cblakley15@gmail.com>
2026-08-31 20:16:36 -0700

    Replaced homegrown hash table for string interning with verstable

--- src/string_pool.c
+++ src/string_pool.c
@@ -22,16 +22,26 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <string.h>
 #include "string_view.h"
 
-#define DEFAULT_TABLE_CAPACITY 64
-#define TABLE_GROWTH_FACTOR 2
+// We call the hash function ourselves, so no need to hash again
+static inline
+uint64_t hash_identity(uint64_t h)
+{
+    return h;
+}
+
+#define NAME token_map
+#define KEY_TY uint64_t
+#define VAL_TY StringToken
+#define HASH_FN hash_identity
+#define CMPR_FN vt_cmpr_integer
+#include "verstable.h"
+
 #define DEFAULT_STRING_POOL_CAPACITY 512 // Must be a power of 2
 
 static char* string_pool;
 static uint32_t string_pool_size;
 static uint32_t string_pool_capacity;
-static StringToken* tokens; // Each token is an index into string_pool (denoting the start of an interned string)
-static uint32_t table_size;
-static uint32_t table_capacity;
+static token_map tokens;
 
 void string_pool_init(void)
 {
@@ -39,33 +49,7 @@ void string_pool_init(void)
     string_pool_capacity = DEFAULT_STRING_POOL_CAPACITY;
     string_pool_size = 1; // Reserves StringToken 0 to represent empty bucket in hash table
 
-    tokens = calloc(DEFAULT_TABLE_CAPACITY, sizeof(*tokens));
-    table_capacity = DEFAULT_TABLE_CAPACITY;
-    table_size = 0;
-}
-
-// FNV-1 hash (32-bit variant)
-static
-uint32_t hash_fnv(StringView s)
-{
-    uint32_t hash = 2166136261;
-    const char* end = s.value + s.len;
-    for(const char* c = s.value; c != end; ++c) {
-        hash *= 16777619;
-        hash ^= downcase_table[(uint8_t)(*c)];
-    }
-    return hash;
-}
-
-static
-uint32_t hash_fnv_c_str(const char* s)
-{
-    uint32_t hash = 2166136261;
-    for(const char* c = s; *c != '\0'; ++c) {
-        hash *= 16777619;
-        hash ^= downcase_table[(uint8_t)(*c)];
-    }
-    return hash;
+    token_map_init(&tokens);
 }
 
 static
@@ -93,55 +77,17 @@ StringToken append(StringView s)
     return token;
 }
 
+// FNV-1 hash (32-bit variant)
 static
-bool insert(StringView s, StringToken* token)
+uint32_t hash_fnv(StringView s)
 {
-    uint32_t hash = hash_fnv(s);
-    uint32_t i = hash % table_capacity;
-    while(tokens[i]) {
-        if(string_view_equal(s, string_pool + tokens[i])) {
-            // String is already present in table
-            *token = tokens[i];
-            return false;
-        }
-        // Collision
-        ++i; // Linear probing
-        i %= table_capacity;
+    uint32_t hash = 2166136261;
+    const char* end = s.value + s.len;
+    for(const char* c = s.value; c != end; ++c) {
+        hash *= 16777619;
+        hash ^= downcase_table[(uint8_t)(*c)];
     }
-    // New string, no collision
-    tokens[i] = append(s);
-    *token = tokens[i];
-    ++table_size;
-    return true;
-}
-
-static
-void reinsert(StringToken token)
-{
-    uint32_t hash = hash_fnv_c_str(string_pool + token);
-    uint32_t i = hash % table_capacity;
-    while(tokens[i]) {
-        // Collision
-        ++i; // Linear probing
-        i %= table_capacity;
-    }
-    tokens[i] = token;
-}
-
-static
-void grow_table(void)
-{
-    uint32_t old_capacity = table_capacity;
-    StringToken* old_tokens = tokens;
-
-    table_capacity *= TABLE_GROWTH_FACTOR;
-    tokens = calloc(table_capacity, sizeof(StringToken));
-    for(uint32_t i = 0; i < old_capacity; ++i) {
-        if(old_tokens[i]) {
-            reinsert(old_tokens[i]);
-        }
-    }
-    free(old_tokens);
+    return hash;
 }
 
 StringToken string_pool_c_str_to_token(const char* s)
@@ -152,12 +98,14 @@ StringToken string_pool_c_str_to_token(const char* s)
 
 StringToken string_pool_to_token(StringView s)
 {
-    StringToken token = 0;
-    if(insert(s, &token) && table_size * 10 / table_capacity >= 7) {
-        // Grow if table is at least 70% full
-        grow_table();
+    uint64_t h = hash_fnv(s);
+    token_map_itr it = token_map_get(&tokens, h);
+    if(token_map_is_end(it)) {
+        StringToken t = append(s);
+        token_map_insert(&tokens, h, t);
+        return t;
     }
-    return token;
+    return it.data->val;
 }
 
 const char* string_pool_to_str(StringToken token)
